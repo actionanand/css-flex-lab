ca0350a951561e06d321d6e46e7954e39d06b779
Anand raja <meetanandns@gmail.com>
2024-01-27 16:31:42 +0000

    Updates

--- assets/index-Vs8_mKua.js
+++ assets/index-Vs8_mKua.js
@@ -1,9 +1,9 @@
 (function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const i of t.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&s(i)}).observe(document,{childList:!0,subtree:!0});function o(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function s(e){if(e.ep)return;e.ep=!0;const t=o(e);fetch(e.href,t)}})();const n=`<div class="flex-container">
-  <div class="item-1">div</div>
-  <div class="item-2">w=250px</div>
-  <div class="item-3">h=250px</div>
-  <div class="item-4">w/h=300px</div>
-  <div class="item-5">w=350px</div>
-  <div class="item-6">w=350px</div>
+  <div class="item-1">1-div</div>
+  <div class="item-2">2-w190</div>
+  <div class="item-3">3-h250</div>
+  <div class="item-4">4-w&h200</div>
+  <div class="item-5">5-w210</div>
+  <div class="item-6">6-w210</div>
 </div>
 `;document.querySelector("#app").innerHTML=n;
